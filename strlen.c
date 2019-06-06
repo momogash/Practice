3ce407f934849399b76bd3ba499e36b7a59b1f27
Moloko MOGASHOA <momogash@c5r6s1.wethinkcode.co.za>
2019-06-06 16:03:10 +0200

    stringlength

--- strlen.c
+++ strlen.c
@@ -4,28 +4,58 @@
 void	ft_putchar(char c);
 void 	ft_putstr(char *str);
 size_t	ft_strlen(char *s);
+void	ft_putnbr(int n);
+
+size_t i;
 
 int main ()
 {
-	char mystr[];
-	mystr[] = "hello";
+	char *mystr="Mashudubele";
 	
-	ft_strlen(mystr);
+	ft_putnbr(ft_strlen(mystr));
 	return (0);
-
 }
 
 
 size_t	ft_strlen(char *s)
 {
-	size_t i;
 
 	i = 0;
 	while (s[i] != '\0')
-	{
 		i++;
 		return(i);
-	}
-
 }
 
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	 i = 0;
+	while (str[i] != '\0')
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
+void		ft_putnbr(int n)
+{
+	if (n == -2147483648)
+		ft_putstr("-2147483648");
+	else if (n < 0)
+	{
+		ft_putchar('-');
+		ft_putnbr(-n);
+	}
+	else if (n >= 10)
+	{
+		ft_putnbr(n / 10);
+		ft_putchar(n % 10 + '0');
+	}
+	else
+		ft_putchar(n + '0');
+}
